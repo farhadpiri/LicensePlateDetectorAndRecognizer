4776051c901756d38c5f408d9bf598c020568c24
mahdi <mahdid.m.2000@gmail.com>
2022-12-28 08:57:02 +0330

    read dataset1 paths

--- DataPreparation/Dataset_Preparation.py
+++ DataPreparation/Dataset_Preparation.py
@@ -1,10 +1,19 @@
+from Config import Addresses
+import os
+import glob
 
 class PrepareDate:
     def __init__(self):
         pass
 
     def read_dataset1(self):
-        pass
+        address = os.path.join(Addresses.datasets_root_address,"datasets/01")
+        images_address = os.path.join(address,"images/*.png")
+        images_path = glob.glob(images_address)
+        annotations_address = os.path.join(address, "annotations/*.xml")
+        annotations_path = glob.glob(annotations_address)
+
+
 
 if(__name__=="__main__"):
     pass
