feabaeda01975aabf9e74a8727bd1490610d960c
mahdi <mahdid.m.2000@gmail.com>
2023-01-08 22:26:39 +0330

    qualize size of dataset

--- datasets/DataPreparation/PrepareForYOLO.py
+++ datasets/DataPreparation/PrepareForYOLO.py
@@ -3,6 +3,7 @@ import random
 import shutil
 
 import cv2
+import numpy as np
 import pandas as pd
 import ntpath
 
@@ -11,12 +12,21 @@ class PrepareForYOLO:
         self.train_folder = os.path.join(os.getcwd(),"YoloDataset/train")
         if(not os.path.isdir(self.train_folder)):
             os.mkdir(self.train_folder)
+            self.train_images_and_labels = os.path.join(self.train_folder,"train.txt")
+        with open(self.train_images_and_labels):
+            pass
         self.val_folder = os.path.join(os.getcwd(),"YoloDataset/val")
         if(not os.path.isdir(self.val_folder)):
             os.mkdir(self.val_folder)
+        self.val_images_and_labels = os.path.join(self.train_folder, "val.txt")
+        with open(self.val_images_and_labels):
+            pass
         self.test_folder = os.path.join(os.getcwd(),"YoloDataset/test")
         if(not os.path.isdir(self.test_folder)):
             os.mkdir(self.test_folder)
+        self.val_images_and_labels = os.path.join(self.train_folder, "val.txt")
+        with open(self.val_images_and_labels):
+            pass
 
     def copy_image(self,path):
         pass
@@ -69,23 +79,7 @@ class PrepareForYOLO:
                 dst_img_path = os.path.join(test_image_dir,tail)
                 dst_txt_path = os.path.join(test_label_dir,f"{name}.txt")
 
-
-            # prepare_output(src_img_path)
-            if(not os.path.isfile(dst_img_path)):
-                shutil.copyfile(src_img_path, dst_img_path)
-
-            img = cv2.imread(src_img_path)
-
-            [H,W,C] = img.shape
-            xmin = int(row["xmin"])
-            xmax = int(row["xmax"])
-            ymax = int(row["ymax"])
-            ymin = int(row["ymin"])
-
-            x_c = int(xmin + (xmax - xmin)/2)
-            y_c = int(ymin + (ymax - ymin)/2)
-            w = (xmax - xmin)/W
-            h = (ymax - ymin)/H
+            [x_c, y_c, w, h] = self.prepare_output(row, src_img_path,dst_img_path,add_label=False)
 
             with open(dst_txt_path, "a+") as f:
                 line = f"0 {x_c} {y_c} {w} {h}\n"
@@ -95,6 +89,40 @@ class PrepareForYOLO:
 
     def read_dataframe(self, path):
         return pd.read_excel(path)
+
+    def prepare_output(self, row, src_img_path,dst_img_path,img_size=720,add_label=False):
+
+
+        img = cv2.imread(src_img_path)
+
+        [H, W, C] = img.shape
+        image_scale = max(H/img_size,W/img_size)
+        img_resized = cv2.resize(img,(int(W/image_scale),int(H/image_scale)))
+
+        width_buttom = int((img_size - img_resized.shape[0])/2)
+        width_up = int((img_size - img_resized.shape[0]+1)/2)
+        width_left = int((img_size - img_resized.shape[1])/2)
+        width_right = int((img_size - img_resized.shape[1]+1)/2)
+
+        img_resized_pdded = np.pad(img_resized, ((width_buttom, width_up), (width_left, width_right), (0, 0)), 'constant',constant_values=0)
+
+        xmin = int(int(row["xmin"])/image_scale + width_left)
+        xmax = int(int(row["xmax"])/image_scale + width_left)
+        ymax = int(int(row["ymax"])/image_scale + width_up)
+        ymin = int(int(row["ymin"])/image_scale + width_up)
+
+
+        x_c = int(xmin + (xmax - xmin) / 2)
+        y_c = int(ymin + (ymax - ymin) / 2)
+        w = (xmax - xmin) / img_size
+        h = (ymax - ymin) / img_size
+
+        if(add_label):
+            img_resized_pdded= cv2.rectangle(img_resized_pdded,(int(x_c-w*img_size/2),int(y_c-h*img_size/2)),(int(x_c+w*img_size/2),int(y_c+h*img_size/2)),(0,0,255),2)
+        cv2.imwrite(dst_img_path,img_resized_pdded)
+
+
+        return [x_c, y_c, w, h]
 
 
 if(__name__=="__main__"):
