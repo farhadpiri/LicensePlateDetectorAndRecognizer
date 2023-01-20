9b568043d2ab9548668510d38c3012a764f4b024
mahdi <mahdid.m.2000@gmail.com>
2023-01-20 12:45:50 +0330

    char acc = 96.43

--- PlateDetector/Models/Deepayan/Model/Models.py
+++ PlateDetector/Models/Deepayan/Model/Models.py
@@ -46,7 +46,7 @@ class CRNN(nn.Module):
                 cnn.add_module('relu{0}'.format(i), nn.ReLU(True))
 
         convRelu(0)
-        cnn.add_module('pooling{0}'.format(0), nn.MaxPool2d(2, 2))  # 64x16x64
+        cnn.add_module('pooling{0}'.format(0), nn.MaxPool2d(3, 3))  # 64x16x64
         convRelu(1)
         cnn.add_module('pooling{0}'.format(1), nn.MaxPool2d(2, 2))  # 128x8x32
         convRelu(2, True)
